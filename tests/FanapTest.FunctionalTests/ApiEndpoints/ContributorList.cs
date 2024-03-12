2bc4d78100000707e98f5e6bfebad4856d7aa898
Reza Najafipajhoh <r.najafipajhoh@Negah.com>
2024-03-12 11:56:48 +0330

    First commit.

---
+++ tests/FanapTest.FunctionalTests/ApiEndpoints/ContributorList.cs
@@ -0,0 +1,22 @@
+﻿using Ardalis.HttpClientTestExtensions;
+using FanapTest.Infrastructure.Data;
+using FanapTest.Web.Contributors;
+using Xunit;
+
+namespace FanapTest.FunctionalTests.ApiEndpoints;
+
+[Collection("Sequential")]
+public class ContributorList(CustomWebApplicationFactory<Program> factory) : IClassFixture<CustomWebApplicationFactory<Program>>
+{
+  private readonly HttpClient _client = factory.CreateClient();
+
+  [Fact]
+  public async Task ReturnsTwoContributors()
+  {
+    var result = await _client.GetAndDeserializeAsync<ContributorListResponse>("/Contributors");
+
+    Assert.Equal(2, result.Contributors.Count);
+    Assert.Contains(result.Contributors, i => i.Name == SeedData.Contributor1.Name);
+    Assert.Contains(result.Contributors, i => i.Name == SeedData.Contributor2.Name);
+  }
+}
